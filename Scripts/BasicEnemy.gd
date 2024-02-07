30f9dc6ae309cde04a1a2853865ea8d994a417d2
PC01 <128602911+SimonCdG@users.noreply.github.com>
2024-02-07 15:36:54 +0100

    jsp

--- Scripts/BasicEnemy.gd
+++ Scripts/BasicEnemy.gd
@@ -11,20 +11,34 @@ signal triggerSignal
 
 @onready var sprite = $Sprite2D
 @onready var deathScene: PackedScene = preload("res://Scenes/Effects/death_effect.tscn")
-
+@onready var timer = $Timer
+var return_to_start = false
+var startpostion: Vector2
 var direction: Vector2
 var vue: bool = false
-
+var attack = false
+var pos_diferse = 20
+func _ready():
+	startpostion = position
+func diferen():
+	return true if abs(startpostion.x) - abs(position.x) < pos_diferse and abs(startpostion.y) - abs(position.y) < pos_diferse else false
 func _physics_process(delta):
-	
 	direction = lerp(direction, Vector2.ZERO, 10 * delta)
-	if vue:
+	if vue and not return_to_start:
 		direction = (Globals.playerposition - position).normalized()
+	elif return_to_start == true:
+		if diferen():
+			return_to_start = false
+		else:
+			direction = (startpostion - position).normalized()
 	
 	velocity = direction * maxSpeed
 	move_and_slide()
 
 func hit(hitPosition: Vector2, damage: int):
+	vue = false
+	attack = true
+	timer.start()
 	direction = (position - hitPosition).normalized() * knockback/10
 	
 	var tween = create_tween()
@@ -59,7 +73,13 @@ func hit(hitPosition: Vector2, damage: int):
 
 func _on_vue_body_entered(body):
 	vue = true
+	return_to_start = false
 
 
 func _on_nomorelook_body_exited(body):
 	vue = false
+	return_to_start = true
+
+
+func _on_timer_timeout():
+	vue = true
